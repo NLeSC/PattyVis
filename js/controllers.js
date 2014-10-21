dda30bedd39f3b209ca56acc48ed851e6a301f61
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-10-21 12:05:17 +0200

    Implement search functionality
    + No results found message

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,17 @@
 var pattyApp = angular.module('pattyApp', []);
 
 pattyApp.controller('SearchCtrl', function ($scope, $http){
-   $scope.results = [];
+   $scope.sites = [];
+   $scope.query = '';
    $http.get('data/sites.json').success(function (data){
-       $scope.results = data.features;
+       $scope.sites = data.features;
    });
+
+   $scope.searchFn = function (value, index){
+       if( !$scope.query ){ return false; }
+       var re = new RegExp($scope.query, 'i');
+       return ( re.test(value.properties.description) || 
+           re.test(value.properties.site_interpretation) || 
+           value.id == $scope.query );
+   }
 });
