841fd6b0e11b75871db84a198a0095935d8a3591
mkuzak <mateusz.kuzak@gmail.com>
2014-10-21 16:38:02 +0200

    Cleanup code

--- js/minimap.js
+++ js/minimap.js
@@ -1,26 +1,3 @@
-var iconFeature = new ol.Feature({
-  geometry: new ol.geom.Point(ol.proj.transform([12.5469185, 41.8286509], 'EPSG:4326', 'EPSG:3857')),
-  name: 'Null Island',
-  population: 4000,
-  rainfall: 500
-});
-
-var iconStyle = new ol.style.Style({
-  image: new ol.style.Icon(({
-    anchor: [0.5, 46],
-    anchorXUnits: 'fraction',
-    anchorYUnits: 'pixels',
-    opacity: 0.75,
-    src: 'data/icon.png'
-  }))
-});
-
-iconFeature.setStyle(iconStyle);
-
-// var vectorSource = new ol.source.Vector({
-//   features: [iconFeature]
-// });
-
 function centerMap(center) {
   map.getView().setCenter(ol.proj.transform(center, 'EPSG:4326', 'EPSG:3857'));
 }
@@ -109,57 +86,9 @@ var rasterLayer = new ol.layer.Tile({
 
 var map = new ol.Map({
   layers: [rasterLayer, vectorLayer],
-  // layers: [vectorLayer, rasterLayer], // Note: order makes a difference, last one is on top
-  // layers: [rasterLayer],
   target: document.getElementById('overhead-map'),
   view: new ol.View({
     center: ol.proj.transform([12.5469185, 41.8286509], 'EPSG:4326', 'EPSG:3857'),
     zoom: 10
   })
 });
-
-
-/*
-var element = document.getElementById('popup');
-
-var popup = new ol.Overlay({
-  element: element,
-  positioning: 'bottom-center',
-  stopEvent: false
-});
-map.addOverlay(popup);
-
-// display popup on click
-map.on('click', function(evt) {
-  var feature = map.forEachFeatureAtPixel(evt.pixel,
-      function(feature, layer) {
-        return feature;
-      });
-  if (feature) {
-    var geometry = feature.getGeometry();
-    var coord = geometry.getCoordinates();
-    popup.setPosition(coord);
-    $(element).popover({
-      'placement': 'top',
-      'html': true,
-      'content': feature.get('name')
-    });
-    $(element).popover('show');
-  } else {
-    $(element).popover('destroy');
-  }
-});
-
-// change mouse cursor when over marker
-$(map.getViewport()).on('mousemove', function(e) {
-  var pixel = map.getEventPixel(e.originalEvent);
-  var hit = map.forEachFeatureAtPixel(pixel, function(feature, layer) {
-    return true;
-  });
-  if (hit) {
-    map.getTarget().style.cursor = 'pointer';
-  } else {
-    map.getTarget().style.cursor = '';
-  }
-});
-*/
