a41b893de1cd1d9d8ff5fd63f3bdee38e7a1e7d8
Patrick Bos <egpbos@gmail.com>
2014-10-21 13:55:44 +0200

    Added example for loading JSON for markers.

--- js/icon.js
+++ js/icon.js
@@ -17,9 +17,58 @@ var iconStyle = new ol.style.Style({
 
 iconFeature.setStyle(iconStyle);
 
-var vectorSource = new ol.source.Vector({
-  features: [iconFeature]
-});
+// var vectorSource = new ol.source.Vector({
+//   features: [iconFeature]
+// });
+
+var vectorSource = new ol.source.GeoJSON(
+    /** @type {olx.source.GeoJSONOptions} */ ({
+      object: {
+        "type": "FeatureCollection",
+        "features": [{
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [12.5464756318, 41.829304, 130]
+          },
+          "id": 162,
+          "properties": {
+            "pointcloud": "data/sites/162/cloud_las.js",
+            "description": "Pyramid",
+            "thumbnail": "data/sites/162/P1120067.JPG",
+            "site_context": "Funerary",
+            "site_interpretation": "Funerary tower",
+            "condition": "Damaged"
+          },
+          "bbox": [12.546372, 41.829228, 121.484, 12.546579, 41.829381, 144.177]
+        }, {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [12.546488, 41.828926, 120.21]
+          },
+          "id": 13,
+          "properties": {
+            "pointcloud": "data/sites/13/cloud_las.js",
+            "description": "Block",
+            "thumbnail": "data/sites/13/IMG_0040.JPG",
+            "site_context": "Unknown",
+            "site_interpretation": "Unknown",
+            "condition": "Chipped"
+          },
+          "bbox":[12.546479, 41.828919, 120, 12.546497, 41.828934, 120.42]
+        }],
+        "crs": {
+          "type":"name",
+          "properties": {
+              "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+              // "name": "EPSG:3857"
+          }
+        }
+      },
+    projection: 'EPSG:3857'
+    }));
+
 
 var vectorLayer = new ol.layer.Vector({
   source: vectorSource
