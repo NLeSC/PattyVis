5e5068a816982ae34cf79a2aee41de20d2722925
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-10-22 09:30:54 +0200

    Add label to scene
    The label is placed at some coordinates and always faces the camera.
    Refs #9

--- js/app.js
+++ js/app.js
@@ -170,6 +170,84 @@ function initThree() {
 	renderer.domElement.addEventListener('click', onClick, false);
 }
 
+function makeTextSprite( message, parameters ){
+    if ( parameters === undefined ) parameters = {};
+
+    var fontface = parameters.hasOwnProperty("fontface") ?
+        parameters["fontface"] : "Arial";
+
+    var fontsize = parameters.hasOwnProperty("fontsize") ?
+        parameters["fontsize"] : 18;
+
+    var borderThickness = parameters.hasOwnProperty("borderThickness") ?
+        parameters["borderThickness"] : 4;
+
+    var borderColor = parameters.hasOwnProperty("borderColor") ?
+        parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
+
+    var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+        parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
+
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    context.font = "Bold " + fontsize + "px " + fontface;
+
+    // get size data (height depends only on font size)
+    var metrics = context.measureText( message );
+    var textWidth = metrics.width;
+
+    // background color
+    context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
+                          + backgroundColor.b + "," + backgroundColor.a + ")";
+
+    context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
+                          + borderColor.b + "," + borderColor.a + ")";
+
+    context.lineWidth = borderThickness;
+    roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+    // 1.4 is extra height factor for text below baseline: g,j,p,q.
+
+    // text color
+    context.fillStyle = "rgba(0, 0, 0, 1.0)";
+
+    context.fillText( message, borderThickness, fontsize + borderThickness);
+
+    // canvas contents will be used for a texture
+    var texture = new THREE.Texture(canvas)
+    texture.needsUpdate = true;
+
+    var spriteMaterial = new THREE.SpriteMaterial(
+        { map: texture, useScreenCoordinates: false,} );
+    var sprite = new THREE.Sprite( spriteMaterial );
+    //sprite.scale.set(100,50,1.0);
+    sprite.scale.set(10, 5, 1.0);
+    return sprite;
+}
+
+// function for drawing rounded rectangles
+function roundRect(ctx, x, y, w, h, r){
+    ctx.beginPath();
+    ctx.moveTo(x+r, y);
+    ctx.lineTo(x+w-r, y);
+    ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+    ctx.lineTo(x+w, y+h-r);
+    ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+    ctx.lineTo(x+r, y+h);
+    ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+    ctx.lineTo(x, y+r);
+    ctx.quadraticCurveTo(x, y, x+r, y);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+}
+
+// add a text label
+function addTextLabel(msg, x, y, z){
+    var label = makeTextSprite(msg);
+    label.position.set(x, y, z);
+    scene.add( label );
+}
+
 function createGrid(width, length, spacing) {
 	var material = new THREE.LineBasicMaterial({
 		color : 0xBBBBBB
@@ -285,6 +363,7 @@ function render() {
 
 initThree();
 initGUI();
+addTextLabel(' Lion ', -1.5, 6.3, -1.3);
 render();
 
 function placeStart() {
@@ -325,6 +404,5 @@ function toggleOculus() {
 	firstperson.enable();
 	
   }
-
   
 }
