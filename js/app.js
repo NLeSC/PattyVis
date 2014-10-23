fb1c6252622696ef2b4e9885e92462a5dc0fedf1
maartenvm <m.vanmeersbergen@esciencecenter.nl>
2014-10-23 15:04:31 +0200

    out_8 restored.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var defaultPointSize = 0.09;
 var defaultLOD = 12;
 
 //var pointcloudPath = 'http://192.168.6.34/potree/resources/pointclouds/viaappia/cloud_laz.js';
-var pointcloudPath = 'data/out_6/cloud.js';
+var pointcloudPath = 'data/out_8/cloud.js';
 
 var pointcloud;
 var skybox;
