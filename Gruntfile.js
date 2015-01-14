3701dbdea0ece4f49d3d2995aa559b85e6d8e2da
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2015-01-14 13:52:45 +0100

    When angular directive templates are changed then reload browser.

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,8 @@ module.exports = function (grunt) {
         files: [
           '<%= yeoman.app %>/{,*/}*.html',
           '.tmp/styles/{,*/}*.css',
-          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.app %>/scripts/{,*/}*.html'
         ]
       }
     },
