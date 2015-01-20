ee195f78dc15ccfa85e9d6d4873152eb619f8cfc
Patrick Bos <egpbos@gmail.com>
2015-01-20 16:40:03 +0100

    Added SiteBox service.
    Added bare service file for SiteBoxService functionality.
    Also included in index.html.

---
+++ app/scripts/pointcloud/site-box.service.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  function SiteBoxService(THREE) {
+    var me = this;
+
+  }
+
+  angular.module('pattyApp.pointcloud')
+    .service('SiteBoxService', SiteBoxService);
+})();
