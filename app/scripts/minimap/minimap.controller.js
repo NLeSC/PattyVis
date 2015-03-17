f0eebe6e2b741918fd1da8d21673972a3a8dad63
Patrick Bos <egpbos@gmail.com>
2015-03-17 16:01:05 +0100

    Changes to minimap.controller for testability
    Took event and messagebus functions out and made them controller members
    to make them testable.

--- app/scripts/minimap/minimap.controller.js
+++ app/scripts/minimap/minimap.controller.js
@@ -7,10 +7,10 @@
     var olProjectionCode = 'EPSG:3857';
     var siteProjectionCode = null;
 
-    this.syncSiteProjectionCode = function() {
+    var syncSiteProjectionCode = function() {
       siteProjectionCode = DrivemapService.getCrs();
     };
-    DrivemapService.ready.then(this.syncSiteProjectionCode);
+    DrivemapService.ready.then(syncSiteProjectionCode);
 
     var siteStyle = new ol.style.Style({
       stroke: new ol.style.Stroke({
@@ -90,18 +90,20 @@
       })
     });
     this.map = map;
-    map.on('rightclick', function(event) {
+
+    this.onMapRightclick = function(event) {
       event.preventDefault();
 
       return false;
-    });
+    };
+
+    map.on('mousedown', this.onMapRightclick);
 
     // TODO set initial location for the map
     // TODO toggle map on/of
 
     // listen on map click
-    map.on('click', function(event) {
-
+    this.onMapClick = function(event) {
       // EPSG:3857 (strange internal OpenLayers lat/lon units)
       var lat = event.coordinate[0];
       var lon = event.coordinate[1];
@@ -118,14 +120,18 @@
         '\nESPG:4326 (google)\nx: ' + lat4326 +
         '\ny: ' + lon4326 + '\n' + siteProjectionCode +' (drivemap)\nx: ' + latlas +
         '\ny: ' + lonlas);
-    });
+    };
+
+    map.on('click', this.onMapClick);
 
     map.addLayer(CamFrustumService.layer);
 
-    Messagebus.subscribe('cameraMoved', function(event, frustum) {
+    this.cameraMovedMessage = function(event, frustum) {
       CamFrustumService.onCameraMove(frustum);
       map.getView().fitExtent(CamFrustumService.getExtent(), map.getSize());
-    });
+    };
+
+    Messagebus.subscribe('cameraMoved', this.cameraMovedMessage);
 
   }
 
