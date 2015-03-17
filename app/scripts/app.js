8d1049317205115415ca115a4f8071fae79d7bcc
maartenvm <m.vanmeersbergen@esciencecenter.nl>
2015-03-17 14:34:44 +0100

    Moved the measuring tools and services to their own module.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,6 +19,7 @@
       'ui.bootstrap',
       'pattyApp.searchbox',
       'pattyApp.minimap',
+      'pattyApp.measuring',
       'pattyApp.pointcloud'
     ])
     .run(function(SitesService, DrivemapService) {
@@ -31,5 +32,6 @@
   angular.module('pattyApp.core', ['pattyApp.utils']);
   angular.module('pattyApp.minimap', ['pattyApp.core']);
   angular.module('pattyApp.pointcloud', ['pattyApp.core']);
+  angular.module('pattyApp.measuring', ['pattyApp.core', 'pattyApp.pointcloud']);
   angular.module('pattyApp.searchbox', ['pattyApp.core', 'pattyApp.pointcloud']);
 })();
