166ed4e6ccb4bc7bb900709a4bf643567ffd94de
Patrick Bos <egpbos@gmail.com>
2015-03-17 11:56:46 +0100

    100% coverage for cam-frustrum.service

--- test/spec/minimap/cam-frustum.service.spec.js
+++ test/spec/minimap/cam-frustum.service.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-fdescribe('minimap.CamFrustumService', function() {
+describe('minimap.CamFrustumService', function() {
 
   // load the module
   beforeEach(module('pattyApp.minimap', 'mockedDrivemap'));
@@ -17,18 +17,18 @@ fdescribe('minimap.CamFrustumService', function() {
     });
   });
 
-  fdescribe('initial state', function() {
-    fit('should have an layer', function() {
+  describe('initial state', function() {
+    it('should have an layer', function() {
       expect(service.layer).toBeDefined();
     });
 
-    fit('should have an camFrustum', function() {
+    it('should have an camFrustum', function() {
       expect(service.camFrustum).toBeDefined();
     });
   });
 
-  fdescribe('onCameraMove() function', function() {
-    fit('should set coordinates for camFrustum', function() {
+  describe('onCameraMove() function', function() {
+    it('should set coordinates for camFrustum', function() {
       var mockFrustum = {'cam':{'x':297051.4777832031,'y':4632724.935546875,'z':141.26402282611974},'left':{'x':297406.5820811396,'y':4632622.404538055,'z':146.29805584894072},'right':{'x':297066.8933021421,'y':4632355.646999936,'z':146.29805584894078}};
       var mockCamFrustumCoordinates = [[1397771.4318921762,5134117.448713819],[1398250.8778260758,5133993.199513802],[1397806.1460583345,5133621.6601744285],[1397771.4318921762,5134117.448713819]];
 
