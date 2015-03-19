28a5d86e1bdadf32ab20d7d356f187d29b33a98f
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-03-19 11:10:43 +0100

    Add toggle buttons for showing and hiding point clouds/meshes
    Add toggle buttons for showing and hiding point clouds/meshes for a
    site. Also displaying more site details.

--- app/scripts/searchbox/searchbox.controller.js
+++ app/scripts/searchbox/searchbox.controller.js
@@ -6,11 +6,59 @@
     this.currentPage = 1;
     this.SitesService = SitesService;
 
+    this.toggleButtons = {
+      _site_pc: true,
+      _drive_map: true,
+      _site_mesh: false
+    };
+
+    Object.defineProperties(this.toggleButtons, {
+      site_pc: {
+        get: function() {
+          return this._site_pc;
+        },
+        set: function(bool) {
+          this._site_pc = bool;
+        },
+        enumerable: true,
+        configurable: true
+      },
+      drive_map: {
+        get: function() {
+          return this._drive_map;
+        },
+        set: function(bool) {
+          this._drive_map = bool;
+        },
+        enumerable: true,
+        configurable: true
+      },
+      site_mesh: {
+        get: function() {
+          return this._site_mesh;
+        },
+        set: function(bool) {
+          this._site_mesh = bool;
+        },
+        enumerable: true,
+        configurable: true
+      }
+    });
+
+    this.reconstruction_mesh = 0;
+
+    SitesService.ready.then(function() {
+      SitesService.query = 'site:162';
+    });
+
     this.lookAtSite = function(site) {
       PointcloudService.lookAtSite(site);
     };
     this.showLabel = function(site) {
       PointcloudService.showLabel(site);
+    };
+    this.enterOrbitMode = function(site) {
+      PointcloudService.enterOrbitMode(site);
     };
     /**
      * jump back to first page when query changes
@@ -18,6 +66,7 @@
     this.onQueryChange = function() {
       this.currentPage = 1;
     };
+
   }
 
   angular.module('pattyApp.searchbox')
