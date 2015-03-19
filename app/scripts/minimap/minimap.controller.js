8f2c6e4bf92855f318f30952d5d4ec891befb72e
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2015-03-19 15:12:15 +0100

    Added maximize minimap button added hover site in minimap shows label.

--- app/scripts/minimap/minimap.controller.js
+++ app/scripts/minimap/minimap.controller.js
@@ -107,19 +107,95 @@
     // TODO toggle map on/of
 
     // setup click on a site
-    var selectClick = new ol.interaction.Select({
-      condition: ol.events.condition.click,
-      layers: [vectorLayer],
-      style: siteStyle
-    });
     this.onFeatureClick = function(event) {
       var feature = event.target.item(0);
       var site = SitesService.getById(feature.getId());
       SitesService.selectSite(site);
     };
-    selectClick.getFeatures().on('add', this.onFeatureClick);
-    this.map.addInteraction(selectClick);
+    this.setupSiteClick = function() {
+      var selectClick = new ol.interaction.Select({
+        condition: ol.events.condition.click,
+        layers: [vectorLayer],
+        style: siteStyle
+      });
+      selectClick.getFeatures().on('add', this.onFeatureClick);
+      this.map.addInteraction(selectClick);
+    };
+    this.setupSiteClick();
 
+    this.setupSiteHover = function() {
+      var siteHover = new ol.interaction.Select({
+        condition: ol.events.condition.mouseMove,
+        layers: [vectorLayer],
+        style: function(feature) {
+          var text = feature.getId();
+          var siteHightlightedStyle = new ol.style.Style({
+            stroke: new ol.style.Stroke({
+              color: '#276BE7',
+              width: 10
+            }),
+            fill: new ol.style.Fill({
+              color: 'rgba(255, 255, 0, 0.1)'
+            }),
+            text: new ol.style.Text({
+              font: '12px Calibri,sans-serif',
+              text: text,
+              fill: new ol.style.Fill({
+                color: '#ccc'
+              }),
+              stroke: new ol.style.Stroke({
+                color: '#276BE7',
+                width: 3
+              })
+            })
+          });
+          return [siteHightlightedStyle];
+        }
+      });
+      this.map.addInteraction(siteHover);
+    };
+    this.setupSiteHover();
+
+    this.setupResizeControl = function() {
+      function ResizeControl(optOptions) {
+        var options = optOptions || {};
+
+        var button = document.createElement('button');
+        button.innerHTML = '<span class="glyphicon glyphicon-resize-full"></span>';
+
+        var this_ = this;
+        var maximized = false;
+        var clickOnResize = function() {
+          maximized = !maximized;
+
+          if (maximized) {
+            this_.getMap().getTarget().classList.add('big-minimap');
+            button.innerHTML = '<span class="glyphicon glyphicon-resize-small"></span>';
+          } else {
+            this_.getMap().getTarget().classList.remove('big-minimap');
+            button.innerHTML = '<span class="glyphicon glyphicon-resize-full"></span>';
+          }
+          this_.getMap().updateSize();
+          me.centerOnVisibleSites();
+        };
+
+        button.addEventListener('click', clickOnResize, false);
+        button.addEventListener('touchstart', clickOnResize, false);
+
+        var element = document.createElement('div');
+        element.className = 'resize-minimap ol-unselectable ol-control';
+        element.appendChild(button);
+
+        ol.control.Control.call(this, {
+          element: element,
+          target: options.target
+        });
+      }
+      ol.inherits(ResizeControl, ol.control.Control);
+
+      this.map.addControl(new ResizeControl());
+    };
+    this.setupResizeControl();
 
     this.map.addLayer(CamFrustumService.layer);
 
