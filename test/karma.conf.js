e6c44c3fb8e17b425a3b631a71b85ef6de29f804
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2015-03-31 11:09:15 +0200

    Re-enable test coverage.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -83,7 +83,7 @@ module.exports = function(config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      //'app/scripts/**/*.js': ['coverage']
+      'app/scripts/**/*.js': ['coverage']
     },
     reporters: ['dots', 'junit', 'coverage', 'html'],
     junitReporter: {
