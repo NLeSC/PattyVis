dc3d4222bdfd627853ee3b6a1dfcedc8d209ac0a
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2015-06-24 18:30:40 +0200

    For unit tests we need to mock webgl.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function(config) {
       // PhantomJS has no function() {}.bind(this) functionality, so polyfill it
       'node_modules/phantomjs-polyfill/bind-polyfill.js',
       'test/polyfills/string.includes.js',
+      'test/polyfills/canvas.webgl.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-animate/angular-animate.js',
@@ -41,6 +42,9 @@ module.exports = function(config) {
       'bower_components/openlayers3/build/ol.js',
       'bower_components/ol3-layerswitcher/src/ol3-layerswitcher.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/3dhop/js/spidergl.js',
+      'bower_components/3dhop/js/presenter.js',
+      'bower_components/3dhop/js/trackball_sphere.js',
       'app/scripts/**/*.js',
       // test for directives need the templates, inside test load pattyApp.templates module to get templates
       '.tmp/template.js',
