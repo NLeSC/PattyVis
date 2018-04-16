e6b6ff0c2ffb14826ec0330e6bc7407bba337292
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-04-16 10:14:45 +0200

    Logos should not be part of filerev

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,7 +69,7 @@ module.exports = function(grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
@@ -210,7 +210,7 @@ module.exports = function(grunt) {
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*',
-          '!<%= yeoman.dist %>/images/ESCIENCE_logo.png'
+          '!<%= yeoman.dist %>/images/*.png',
         ]
       }
     },
